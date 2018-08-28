e85e78f9d66a99494bbb70daf5d6b339d8219f99
universalhenry <lhc719215876@162.com>
2018-08-28 21:15:12 +0800

    git commit -m "C++ 20180822"

---
+++ C++/try/main1.cpp
@@ -0,0 +1,16 @@
+/* not working */
+// #include <iostream>
+// using namespace std;
+
+// auto rt(int a)
+// {
+//     if(a >=0) {return ('1');}
+//     else {return -1;}
+// }
+
+// int main()
+// {
+//     rt(1);
+//     rt(-1);
+//     return 0;
+// }

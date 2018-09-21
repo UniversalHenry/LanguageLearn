8469da30e506d3ce169976566c3ed177ef6f77a1
universalhenry <lhc719215876@162.com>
2018-09-22 02:34:18 +0800

    20180921

--- C++/SJTU_course/week2/homework/main1.cpp
+++ C++/SJTU_course/week2/homework/main1.cpp
@@ -6,63 +6,61 @@ using namespace std;
 
 int main(void)
 {
+  //initialization
   long M = 1, N = 1;
   cin >> M >> N;
-  long rec = 0, tmprec = 0;
-
-  if (N < 1e6)
-  {
-    bool flag[N + 1];
-    for (long i = 0; i <= N; i++)flag[i] = true;
-    for (long i = 2; i < sqrt(N) + 1; i++)if (flag[i])for (long j = 2 * i; j <= N; j += i)flag[j] = false;
-    for (long i = M;;){
-      if (flag[i]){
-        while (true){
-          i++;tmprec++;
-          if (flag[i] || i >= N)break;
+  int rec = 0, tmprec = 0;
+  const int t = N-M+1;
+  bool flag[t];
+  //compute prime between M & N
+  if(M<=1e6 || t-1<100){
+    const int n = int(sqrt(N))+1;
+    bool tmp;
+    for(int j = 0; j < t;j++)flag[j]=false;
+    if(M%2==1){for(int j = 0; j < t;j+=2){tmp=true;for(int i = 3; i < n; i+=2){if((M+j)%i==0&&(M+j)/i>1){tmp = false;break;}}flag[j]=tmp;}}
+    else{for(int j = 1; j < t;j+=2){tmp=true;for(int i = 3; i < n; i+=2){if((M+j)%i==0&&(M+j)/i>1){tmp = false;break;}}flag[j]=tmp;}}
+    if(M==1){flag[0]=false;flag[1]=true;}
+    if(M==2){flag[0]=true;}
+  }else{
+    const int n = int(sqrt(t))+1;
+    const long n1 = int(sqrt(N))+1;
+    bool prime[n];
+    for(int i = 2; i < n; i++)prime[i]=true;
+    for(int j = 0; j < t;j++)flag[j]=true;
+    //compute basic prime
+    for(int i = 2; i < n; i++){if(prime[i])for (int j = 2 * i; j < n; j += i)prime[j] = false;}
+    //compute prime between M & N
+    for(int j = 0; j < t;j++)for(int i = 2; i < n;i++)if(flag[j] && prime[i] && (j+M)%i ==0 && (j+M)/i >1){
+      int k = j;
+      while(true){
+        flag[k]=false;
+        k+=i;
+        if(k>=t)break;
         }
-      }
-      else{i++;}
-      if (flag[N]){if(tmprec > rec && tmprec > 1)rec = tmprec;}
-      else{if (i < N)if (tmprec > rec && tmprec > 1)rec = tmprec;}
-      tmprec = 0;
-      if (i >= N)break;
     }
-  }
-  else
-  {
-    const long c = 1e6;
-    const long n = long(sqrt(N));
-    bool *prime[n / c + 1 ];
-    for(long i = 0;i <n / c + 1;i++)prime[i]=new bool[c];
-    bool flag[N-M+1];
-    for(long i = 0; i < n; i++)prime[i/c][i%c]=true;
-    for(long i = 2; i < n; i++){if(prime[i/c][i%c])for (long j = 2 * i; j < n; j += i)prime[j/c][j%c] = false;}
-
-    for(long j = 0; j < N-M+1;j++){
-      for(long i = 2; i < n; i++){
-        if (prime[i/c][i%c]){if((M+j)%i==0){flag[j]=false;break;}}
-      }
-    }
-
-    for (long i = 0;;){
-      if (flag[i]){
-        while (true){
-          i++;tmprec++;
-          if (flag[i] || i >= N-M+1)break;
-        }
-      }
-      else{i++;}
-      if(flag[N-M+1]){if (tmprec > rec && tmprec > 1)rec = tmprec;}
-      else{if (i < N-M+1)if (tmprec > rec && tmprec > 1)rec = tmprec;}
-      tmprec = 0;
-      if (i >= N-M+1)break;
-    }
+    bool tmp ;
+    if(M%2==1){for(int j = 0; j < t;j+=2)if(flag[j]){tmp=true;for(long i = 3; i < n1; i+=2){if((M+j)%i==0){tmp = false;break;}}flag[j]=tmp;}}
+    else{for(int j = 1; j < t;j+=2)if(flag[j]){tmp=true;for(long i = 3; i < n1; i+=2){if((M+j)%i==0){tmp = false;break;}}flag[j]=tmp;}}
   }
 
+  //compute distance
+  int i = 0;
+  while(true){
+    if (flag[i]){
+      while (true){
+        i++;tmprec++;
+        if (flag[i] || i >= t)break;
+      }
+    }else{i++;}
+    if(flag[t-1] && flag[0]){if (tmprec > rec) rec = tmprec;}
+    else if(flag[t-1] && !flag[0]){if (tmprec!=t-1 && tmprec > rec) rec = tmprec;}
+    else{if(i < t)if(tmprec > rec)rec = tmprec;}
+    tmprec = 0;
+    if (i >= t)break;
+  }
   cout << rec;
 
-  cout << "Program finished.";
+  cout << "\nProgram finished.";
   int test;
   cin >> test;
   return 0;

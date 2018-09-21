acbfde22fbeffad5abfa12e162d9ed773c4106e0
root <root@localhost>
2018-09-21 21:26:43 +0800

    20180920

---
+++ C++/try/main3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int main(void) {
+  long M = 1, N = 1;
+  cin >> M >> N;
+  long rec = 0, tmprec = 0;
+  bool flag[N + 1];
+  //for(long i = 0; i<=N;i++)flag[i]=true;
+    
+    cout << "Program finished.";
+    int test;
+    cin >> test;
+    return 0;
+  }

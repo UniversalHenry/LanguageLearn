2e8009999cfb269bce266e4edbdd9eb13795e070
root <root@localhost>
2018-10-16 20:09:37 +0800

    RandomWriter init

---
+++ C++/SJTU_course/week6/lab2/RandomWriter.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib> 
+#include <cstdio>
+#include <fstream>
+#include <vector>
+#include <cmath>
+#include <sstream>
+#include <set>
+#include <map>
+#include <ctime>
+#include <cstring>
+
+using namespace std;
+
+int main(void){
+// time start
+    double dur;
+    clock_t start,end;
+    start = clock();
+
+// main function here
+
+    
+// time stop
+    end = clock();
+    dur = (double)(end - start);
+    printf("\nUseTime:%f",(dur/CLOCKS_PER_SEC));
+	cout << "\nProgram finished.";
+	getchar();
+    return 0;
+}

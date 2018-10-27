d3cdeabf59c8812be24656618e96c7fa60e42757
universalhenry <lhc719215876@163.com>
2018-10-27 19:15:33 +0800

    233

--- C++/SJTU_course/week7/homework/RedundantParentheses.cpp
+++ C++/SJTU_course/week7/homework/RedundantParentheses.cpp
@@ -13,16 +13,16 @@ int get_level(const char &c);
 
 string get_output(const int &front_level);
 
-int main(void){
+int main(void){ // main function here
     string output;
     while(true){
-        output = get_output(0);
+        output = get_output(0); // 0 represets the front level is the lowest, see the implementation
         cout << output;
     }
     return 0;
 }
 
-int get_level(const char &c){
+int get_level(const char &c){ // decide the level of operator
     switch(c){
         case '+':return 1;
         case '-':return 2;
@@ -32,9 +32,9 @@ int get_level(const char &c){
     }
 }
 
-string get_output(const int &front_level){
+string get_output(const int &front_level){ // get the output of the input
     string output = "";
-    int now_level = 5, tmp_level = 0;
+    int now_level = 5, tmp_level = 0; // set the init level, 5 is the highest and 0 is the lowest
     char token;
     while(cin.get(token)){
         switch(token){
@@ -51,7 +51,7 @@ string get_output(const int &front_level){
                 output += token;
                 break;
             }
-            case ')':{
+            case ')':{ // if ), decide whether to output ()
                 cin.get(token);
                 cin.unget();
                 tmp_level = get_level(token);
@@ -59,10 +59,10 @@ string get_output(const int &front_level){
                     return ("(" + output + ")");
                 else return output;
             }
-            case '\n':{
+            case '\n':{// if '\n', output result
                 return (output + token);
             }
-            default:
+            default:// for variables only read in alpha and digit
                 if(isalpha(token) || isdigit(token)) output += token;
         }
     }

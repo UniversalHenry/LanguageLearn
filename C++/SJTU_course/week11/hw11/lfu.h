30bfb13d3f354f01c08d1ddc8995a43b56c2fb0f
universalhenry <lhc719215876@163.com>
2018-11-27 23:46:30 +0800

    233

--- C++/SJTU_course/week11/hw11/lfu.h
+++ C++/SJTU_course/week11/hw11/lfu.h
@@ -13,10 +13,10 @@ public:
     int get(int key) {
         // TODO: your code here
         if (cache.find(key) == cache.end()) return -1;
-        freq[cache[key].second].erase(iter[key]);
+        freq[cache[key].second].erase(freq_record[key]);
         ++cache[key].second;
         freq[cache[key].second].push_back(key);
-        iter[key] = --freq[cache[key].second].end();
+        freq_record[key] = --freq[cache[key].second].end();
         if (freq[minFreq].size() == 0) ++minFreq;
         return cache[key].first;
     }
@@ -30,18 +30,18 @@ public:
         }
         if (cache.size() >= cap) {
             cache.erase(freq[minFreq].front());
-            iter.erase(freq[minFreq].front());
+            freq_record.erase(freq[minFreq].front());
             freq[minFreq].pop_front();
         }
-        cache[key] = {value, 1};
+        cache[key] = make_pair(value, 1);
         freq[1].push_back(key);
-        iter[key] = --freq[1].end();
+        freq_record[key] = --freq[1].end();
         minFreq = 1;
     }
 
 private:
     int cap, minFreq;
-    unordered_map<int, pair<int, int>> cache;
-    unordered_map<int, list<int>> freq;
-    unordered_map<int, list<int>::iterator> iter;
+    unordered_map<int, pair<int, int> > cache;
+    unordered_map<int, list<int> > freq;
+    unordered_map<int, list<int>::iterator> freq_record;
 };
